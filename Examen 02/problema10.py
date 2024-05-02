fa44570f41e5fda65ab62b1916b1c111f6e7788d
luisdamianmat <167490898+luisdamianmat@users.noreply.github.com>
2024-05-01 20:05:57 -0500

    Update pb12.py

--- Examen 02/problema10.py
+++ Examen 02/problema10.py
@@ -1,4 +1,2 @@
 """creamos la variable y agregamos elementos al key del diccionario"""
 dicc={"Nombre":"Luis","Carrera":"Ingenieria Industrial","Año de nacimiento":2000}
-"""imprimimos el diccionario"""
-print("El contenido del diccionario es: {}".format(dicc))
